23f7fd481351be2b2319ae3c38a3675dcf2cc5b2
Yasu <barutanseven@gmail.com>
2025-09-23 14:53:27 +0900

    コンタクトページなど

--- app/mypage/about/page.tsx
+++ app/mypage/about/page.tsx
@@ -1,1 +1,13 @@
-
+export default function AboutPage() {
+    return(
+        <section className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 p-8">
+         <div className="max-w-4xl mx-auto pt-20">
+          <h1 className="text-4xl font-bold text-white mb-8">about me</h1>
+          <div className="text-gray-300 space-y-4">
+            <p>ここに自己紹介を。</p>
+            <p>経歴やスキル、興味のある分野の追加</p>
+          </div>
+         </div>
+        </section>
+    )
+}
---
+++ app/mypage/contact/page.tsx
@@ -0,0 +1,16 @@
+export default function ContactPage() {
+    return (
+        <section id="contact">
+        <section className="min-h-screen bg-gradient-to-b from-gray-800 to-gray-700 p-8">
+            <div className="max-w-4xl mx-auto pt-20">
+                <h1 className="text-4xl font-bold text-white mb-8">Contact</h1>
+
+                <div className="text-gray-300 space-y-4">
+                    <p>お問い合わせはこちらから</p>
+
+                </div>
+            </div>
+        </section>
+         </section>
+    )
+}
